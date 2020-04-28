33cc246a4929f9f77bf790ef092070a0cfd0f7f3
esfraner <francisco.encinas.ruiz@gmail.com>
2020-04-28 14:07:10 +0200

    frat(player-list): added api functions

--- Cliente/src/app/player-list/player.service.ts
+++ Cliente/src/app/player-list/player.service.ts
@@ -1,11 +1,35 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { Player } from "src/models/player";
 @Injectable()
 export class PlayerService {
+  URL = "http://localhost/api/";
   constructor(private http: HttpClient) {}
 
-  fetchPlayers(): Observable<Object> {
-    return this.http.get("/assets/LISTA_PRODUCTOS.json");
+  selectPlayer(idPlayer: number) {
+    return this.http.get(`${this.URL}SelectPLayer.php?idPlayer=${idPlayer}`);
+  }
+
+  getPlayers(): Observable<Object> {
+    return this.http.get(`${this.URL}GetPlayers.php`);
+  }
+
+  createPlayer(player: Player) {
+    return this.http.post(
+      `${this.URL}CreatePlayer.php`,
+      JSON.stringify(player)
+    );
+  }
+
+  removePlayer(idPlayer: number) {
+    return this.http.get(`${this.URL}RemovePlayer.php?idPlayer=${idPlayer}`);
+  }
+
+  updateUser(player: Player) {
+    return this.http.post(
+      `${this.URL}UpdatePlayer.php`,
+      JSON.stringify(player)
+    );
   }
 }
