fca12894d985c9621894f21b4ae09873d0ebbf79
esfraner <francisco.encinas.ruiz@gmail.com>
2020-05-02 10:51:16 +0200

    changed DB name

--- Cliente/src/assets/api/getPlayers.php
+++ Cliente/src/assets/api/getPlayers.php
@@ -1,13 +1,16 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+
 ini_set('error_reporting', E_ALL ^ E_NOTICE ^ E_WARNING);
 ini_set('display_errors', 'on');
 ini_set('max_execution_time', 10);
 require_once __DIR__ . '/oConexion.class.php';
 
-$oConexion = new oConexion('franer.team', 'PI', 'remoto', 'malagaserade2Bproximamente');
+$oConexion = new oConexion('franer.team', 'PI2', 'remoto', 'malagaserade2Bproximamente');
 $oConexion->abrir();
 $oConni = $oConexion->obtenerConexion();
-$stmt = $oConni->prepare('SELECT * FROM JUGADORES');
+$stmt = $oConni->prepare('SELECT * FROM JUGADORES limit 10');
 $stmt->execute();
 $stmt->store_result();
 $stmt->bind_result($id, $name, $birthday, $age, $birthPlace, $birthCountry, $position, $photo);
