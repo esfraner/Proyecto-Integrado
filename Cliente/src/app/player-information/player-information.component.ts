5d8eb0663346a295fc6550a8fd0b4506f0571aff
esfraner <francisco.encinas.ruiz@gmail.com>
2020-05-03 21:36:00 +0200

    feat(player-information): added example form

--- Cliente/src/app/player-information/player-information.component.ts
+++ Cliente/src/app/player-information/player-information.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-player-information',
-  templateUrl: './player-information.component.html',
-  styleUrls: ['./player-information.component.css']
+  selector: "app-player-information",
+  templateUrl: "./player-information.component.html",
+  styleUrls: ["./player-information.component.css"],
 })
 export class PlayerInformationComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
