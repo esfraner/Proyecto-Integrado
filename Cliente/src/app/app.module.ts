797afcb43befc29e7dc3d1108fcc1a52fc95428b
esfraner <francisco.encinas.ruiz@gmail.com>
2020-05-06 14:20:54 +0200

    chore(modules): added form,input,select modules

--- Cliente/src/app/app.module.ts
+++ Cliente/src/app/app.module.ts
@@ -11,6 +11,9 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatDividerModule } from "@angular/material/divider";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
 
 import { MenuNavbarComponent } from "./menu-navbar/menu-navbar.component";
 import { PlayerInformationComponent } from "./player-information/player-information.component";
@@ -32,6 +35,9 @@ import { PlayerService } from "./player-list/player.service";
     MatToolbarModule,
     MatButtonModule,
     MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
   ],
   providers: [PlayerService],
   bootstrap: [AppComponent],
