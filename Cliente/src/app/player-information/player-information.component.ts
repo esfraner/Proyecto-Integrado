bb6e9bacc5218f50b24edf8a0b61c676163de683
esfraner <francisco.encinas.ruiz@gmail.com>
2020-05-06 14:22:46 +0200

    feat(information): added form to information component

--- Cliente/src/app/player-information/player-information.component.ts
+++ Cliente/src/app/player-information/player-information.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-
 @Component({
   selector: "app-player-information",
   templateUrl: "./player-information.component.html",
@@ -7,6 +6,17 @@ import { Component, OnInit } from "@angular/core";
 })
 export class PlayerInformationComponent implements OnInit {
   constructor() {}
-
+  demarcaciones: String[] = [
+    "Portero",
+    "Defensa",
+    "Central",
+    "Lateral derecho",
+    "Lateral izquierdo",
+    "Centrocampista",
+    "Delantero",
+    "Delantero centro",
+    "Extremo derecho",
+    "Extremo izquierdo",
+  ];
   ngOnInit(): void {}
 }
