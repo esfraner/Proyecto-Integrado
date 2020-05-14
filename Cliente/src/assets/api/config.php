486085a86a8d4de903326dab5995a4b4df5fad01
esfraner <francisco.encinas.ruiz@gmail.com>
2020-05-14 12:44:09 +0200

    refavtor(config): added config and created constants

---
+++ Cliente/src/assets/api/config.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__ . '/oConexion.class.php';
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+
+ini_set('error_reporting', E_ALL ^ E_NOTICE ^ E_WARNING);
+ini_set('display_errors', 'on');
+ini_set('max_execution_time', 10);
+
+define("HOST", "franer.team");
+define("BD", "PI2");
+define("USER", "remoto");
+define("PASS", "malagaserade2Bproximamente");
--- Cliente/src/assets/api/getPlayers.php
+++ Cliente/src/assets/api/getPlayers.php
@@ -1,13 +1,7 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+require_once __DIR__ . '/config.php';
 
-ini_set('error_reporting', E_ALL ^ E_NOTICE ^ E_WARNING);
-ini_set('display_errors', 'on');
-ini_set('max_execution_time', 10);
-require_once __DIR__ . '/oConexion.class.php';
-
-$oConexion = new oConexion('franer.team', 'PI2', 'remoto', 'malagaserade2Bproximamente');
+$oConexion = new oConexion(HOST, BD, USER, PASS );
 $oConexion->abrir();
 $oConni = $oConexion->obtenerConexion();
 $stmt = $oConni->prepare('SELECT * FROM JUGADORES limit 10');
@@ -15,8 +9,10 @@ $stmt->execute();
 $stmt->store_result();
 $stmt->bind_result($id, $name, $birthday, $age, $birthPlace, $birthCountry, $position, $photo);
 while ($stmt->fetch()) {
-    $result[] = ["id" => $id, "nombreCompleto" => $name, "fechaNacimiento" => $birthday, "edad" => $age, "lugarNacimiento" => $birthPlace, "paisNacimiento" => $birthCountry, "demarcacion" => $position, "foto" => $photo];
+    $result[] = ["id" => $id, "nombreCompleto" => $name, "fechaNacimiento" => $birthday,
+    "edad" => $age, "lugarNacimiento" => $birthPlace, "paisNacimiento" => $birthCountry,
+    "demarcacion" => $position, "foto" => $photo];
 }
 echo json_encode($result);
-header('Content-Type: application/json');
+//header('Content-Type: application/json');
 $stmt->close();
