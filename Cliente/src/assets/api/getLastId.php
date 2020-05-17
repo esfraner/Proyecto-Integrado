b74d7c5bbeeb2a4a78786f74726f09f1f0c32bb3
esfraner <francisco.encinas.ruiz@gmail.com>
2020-05-17 22:28:57 +0200

    feat(api): added getLastId

---
+++ Cliente/src/assets/api/getLastId.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__ . '/config.php';
+
+$oConexion = new oConexion(HOST, BD, USER, PASS );
+$oConexion->abrir();
+$oConni = $oConexion->obtenerConexion();
+$stmt = $oConni->prepare('SELECT ID from JUGADORES ORDER BY id DESC LIMIT 1;');
+$stmt->execute();
+$stmt->store_result();
+$stmt->bind_result($maxId);
+if ($stmt->fetch()) {
+  echo ++$maxId;
+}
+$stmt->close();
