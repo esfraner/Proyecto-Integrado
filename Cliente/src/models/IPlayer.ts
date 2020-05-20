20436909054775de0e0e486afe6af51b4e782d3f
esfraner <francisco.encinas.ruiz@gmail.com>
2020-05-20 19:35:08 +0200

    feat(Iplayer): created player interface

---
+++ Cliente/src/models/IPlayer.ts
@@ -0,0 +1,10 @@
+export interface IPlayer {
+  id: string;
+  nombreCompleto: string;
+  fechaNacimiento: string;
+  edad: string;
+  lugarNacimiento: string;
+  paisNacimiento: string;
+  demarcacion: string;
+  foto: string;
+}
