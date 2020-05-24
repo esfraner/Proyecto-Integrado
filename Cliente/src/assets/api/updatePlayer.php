629cdb0fe3a9e51eca5d2664034d1c0497c11e5c
esfraner <francisco.encinas.ruiz@gmail.com>
2020-05-24 12:39:21 +0200

    feat(api): created updatePlayer

---
+++ Cliente/src/assets/api/updatePlayer.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/oConexionPDO.class.php';
+
+$postdata = json_decode(file_get_contents("php://input"));
+
+$oConexion = new oConexionPDO(["servidor" => HOST, "baseDatos" => BD, "usuario" => USER, "clave" => PASS]);
+$oConni = $oConexion->obtenerConexion();
+
+$stmt = $oConni->prepare("UPDATE JUGADORES SET `Nombre completo`=?, `Fecha de nacimiento`=?, Edad=?,"
+." `Lugar de nacimiento`=?, `País de nacimiento`=?, Demarcación=?, Foto=? WHERE ID=?;");
+
+if($stmt->execute([ $postdata->nombreCompleto, $postdata->fechaNacimiento,
+$postdata->edad, $postdata->lugarNacimiento, $postdata->paisNacimiento,
+ $postdata->demarcacion, $postdata->foto,$postdata->id])){
+  echo(true);
+}
+else{
+  echo(false);
+}
+
+
