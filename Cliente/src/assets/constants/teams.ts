98685cce4a6b9a664ef7dbd94dd78c62cd396e4e
esfraner <francisco.encinas.ruiz@gmail.com>
2020-06-12 14:15:27 +0200

    chore(constants): added constants teams

---
+++ Cliente/src/assets/constants/teams.ts
@@ -0,0 +1,19 @@
+export let TEAMS = [
+  { teamName: "Barcelona", src: "../assets/images/fcbarcelona.png", number: 1 },
+  { teamName: "Real Madrid", src: "../assets/images/rmadrid.png", number: 2 },
+  { teamName: "Alavés", src: "../assets/images/alaves.png", number: 3 },
+  {
+    teamName: "Athletic Club",
+    src: "../assets/images/athletic.png",
+    number: 6,
+  },
+  {
+    teamName: "Atlético de Madrid",
+    src: "../assets/images/atlmadrid.png",
+    number: 7,
+  },
+  { teamName: "Betis", src: "../assets/images/betis.png", number: 8 },
+  { teamName: "Valencia", src: "../assets/images/valencia.png", number: 33 },
+  { teamName: "Sevilla", src: "../assets/images/sevilla.png", number: 36 },
+  { teamName: "Espanyol", src: "../assets/images/espanyol.png", number: 14 },
+];
