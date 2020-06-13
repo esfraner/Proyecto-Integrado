4eb2efe1c156548a7315c4b99fc22ebdd4d97f42
esfraner <francisco.encinas.ruiz@gmail.com>
2020-06-13 19:17:56 +0200

    refactor(menu): code refactorization

--- Cliente/src/app/menu-navbar/menu-navbar.component.ts
+++ Cliente/src/app/menu-navbar/menu-navbar.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { TEAMS } from "src/assets/constants/teams";
+import { ITeams } from "src/models/ITeams";
+import { IPlayer } from "src/models/IPlayer";
 
 @Component({
-  selector: 'app-menu-navbar',
-  templateUrl: './menu-navbar.component.html',
-  styleUrls: ['./menu-navbar.component.css']
+  selector: "app-menu-navbar",
+  templateUrl: "./menu-navbar.component.html",
+  styleUrls: ["./menu-navbar.component.css"],
 })
 export class MenuNavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  teams: Array<ITeams>;
+  @Output() eventGetPlayers = new EventEmitter<string>();
+  constructor() {
+    this.teams = TEAMS;
   }
 
+  selectTeam(teamName: string) {
+    this.eventGetPlayers.emit(teamName);
+  }
+
+  ngOnInit(): void {}
 }
