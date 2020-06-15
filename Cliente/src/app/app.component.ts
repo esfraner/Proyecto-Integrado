b2ab45b30dfac44dc8c7aa9d731a4031df0e7c0e
esfraner <francisco.encinas.ruiz@gmail.com>
2020-06-16 01:01:27 +0200

    feat(app): added sorting functions

--- Cliente/src/app/app.component.ts
+++ Cliente/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, Output } from "@angular/core";
 import { PlayerService } from "src/services/player.service";
+import { NgxSpinnerService } from "ngx-spinner";
+import { mergeMap, map } from "rxjs/operators";
+import { Player } from "src/models/player";
 
 @Component({
   selector: "app-root",
@@ -12,11 +15,56 @@ export class AppComponent {
   @Output()
   players$ = this.playerService.getPlayers(this.team);
 
-  constructor(private playerService: PlayerService) {}
+  isAscendingId: boolean = false;
+  isAscendingName: boolean = false;
+  isAscendingAge: boolean = false;
 
-  ngOnInit(): void {}
+  constructor(
+    private playerService: PlayerService,
+    private spinner: NgxSpinnerService
+  ) {}
+
+  ngOnInit(): void {
+    this.showLoadSpinner();
+  }
+
+  ngAfterViewInit(): void {
+    this.hideLoadSpinner();
+  }
 
   selectTeam(team: string) {
+    this.showLoadSpinner();
     this.players$ = this.playerService.getPlayers(team);
+    this.hideLoadSpinner();
+  }
+
+  showLoadSpinner() {
+    this.spinner.show();
+  }
+
+  hideLoadSpinner() {
+    setTimeout(() => {
+      this.spinner.hide();
+    }, 950);
+  }
+
+  sortPlayersByProperty(property, isAscendent) {
+    this.players$ = this.players$.pipe(
+      map((players: Player[]) => {
+        return players.sort((a, b) =>
+          isAscendent
+            ? this.ascendentSorting(a, b, property)
+            : this.descendentSorting(a, b, property)
+        );
+      })
+    );
+  }
+
+  ascendentSorting(a, b, property) {
+    return a[property] < b[property] ? -1 : 1;
+  }
+
+  descendentSorting(a, b, property) {
+    return a[property] > b[property] ? -1 : 1;
   }
 }
